ad11b5a06606720d17e45373d40ade4df7e20671
jin lezama <jingoto22@gmail.com>
2020-04-10 19:10:29 -0600

    fixed signin and signout auth

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,13 +5,15 @@ import SignedOutLinks from './SignedOutLinks';
 import { connect } from 'react-redux'
 
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const { auth } = props;
+    // console.log(auth);
+    const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
     return (
        <nav className="nav-wrapper blue darken-3">
            <div className="container">
                <Link to='/' className="brand-logo">JINEIDY</Link>
-               <SignedInLinks />
-               <SignedOutLinks />
+               { links }
            </div>
        </nav>
     )
@@ -19,7 +21,7 @@ const Navbar = () => {
 
 const mapStateToProps = (state) => {
     return {
-
+        auth: state.firebase.auth
     }
 }
 
